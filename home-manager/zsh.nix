17348895fa0020b28a8ec37f0d8543c2850d1540
Ha The Tung <tunght13488@gmail.com>
2026-04-02 16:36:03 +0000

    feat: add starship

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -8,72 +8,79 @@
 {
   programs.zsh = {
     enable = true;
-    prezto = {
-      enable = true;
-      caseSensitive = false;
-      color = true;
-      pmodules = [
-        "environment"
-        "terminal"
-        "editor"
-        "history"
-        "directory"
-        "spectrum"
-        "utility"
-        "completion"
-        "archive"
-        "command-not-found"
-        "dpkg"
-        "pacman"
-        "fasd"
-        "git"
-        "ruby"
-        "homebrew"
-        "docker"
-        # "explain"
-        "node"
-        "osx"
-        "yum"
-        "ssh"
-        "tmux"
-        "syntax-highlighting"
-        "history-substring-search"
-        # "fzf"
-        # "contrib-prompt"
-        "prompt"
+  };
+
+  programs.zsh.prezto = {
+    enable = true;
+    caseSensitive = false;
+    color = true;
+    pmodules = [
+      "environment"
+      "terminal"
+      "editor"
+      "history"
+      "directory"
+      "spectrum"
+      "utility"
+      "completion"
+      "archive"
+      "command-not-found"
+      "dpkg"
+      "pacman"
+      "fasd"
+      "git"
+      "ruby"
+      "homebrew"
+      "docker"
+      # "explain"
+      "node"
+      "osx"
+      "yum"
+      "ssh"
+      "tmux"
+      "syntax-highlighting"
+      "history-substring-search"
+      # "fzf"
+      # "contrib-prompt"
+      "prompt"
+    ];
+    editor = {
+      keymap = "vi";
+      dotExpansion = true;
+      promptContext = true;
+    };
+    ssh.identities = [
+      "ssh_sl"
+      "ssh_tunght13488"
+    ];
+    syntaxHighlighting = {
+      highlighters = [
+        "main"
+        "brackets"
+        "pattern"
+        "line"
+        "cursor"
+        "root"
       ];
-      editor = {
-        keymap = "vi";
-        dotExpansion = true;
-        promptContext = true;
+      styles = {
+        builtin = "bg=blue";
+        command = "bg=blue";
+        function = "bg=blue";
       };
-      ssh.identities = [
-        "ssh_sl"
-        "ssh_tunght13488"
-      ];
-      syntaxHighlighting = {
-        highlighters = [
-          "main"
-          "brackets"
-          "pattern"
-          "line"
-          "cursor"
-          "root"
-        ];
-        styles = {
-          builtin = "bg=blue";
-          command = "bg=blue";
-          function = "bg=blue";
-        };
-        pattern = {
-          "rm*-rf*" = "fg=white,bold,bg=red";
-        };
-      };
-      terminal = {
-        autoTitle = true;
-        windowTitleFormat = "%n@%m: %s";
-        tabTitleFormat = "%m: %s";
+      pattern = {
+        "rm*-rf*" = "fg=white,bold,bg=red";
       };
     };
+    terminal = {
+      autoTitle = true;
+      windowTitleFormat = "%n@%m: %s";
+      tabTitleFormat = "%m: %s";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = { };
   };
 }
