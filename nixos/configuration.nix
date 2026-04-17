201fbf1e72958481a157b55c18c674c236f4722b
Ha The Tung <tunght13488@gmail.com>
2026-04-17 18:01:14 +0800

    feat: install latest 1password

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,9 +142,13 @@
   programs.zsh.enable = true;
   programs.git.enable = true;
 
-  programs._1password.enable = true;
+  programs._1password = {
+    enable = true;
+    package = pkgs.unstable._1password-cli;
+  };
   programs._1password-gui = {
     enable = true;
+    package = pkgs.unstable._1password-gui;
     # Certain features, including CLI integration and system authentication support,
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "tung" ];
